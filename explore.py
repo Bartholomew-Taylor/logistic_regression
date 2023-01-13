02b495c1d33446b0c8933181aa21f7ddf64a1496
Bart Taylor <bartholomew.s.taylor@gmail.com>
2023-01-13 16:52:33 -0600

    end of day push

--- explore.py
+++ explore.py
@@ -1,4 +1,25 @@
 # explorer stuff for zillow
+from sklearn.metrics import precision_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import classification_report
+import pandas as pd
+from scipy import stats
+from pydataset import data
+import numpy as np
+import env
+import matplotlib.pyplot as plt
+import os
+import prepare
+import wrangle
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+import explore
+
 
 def plot_variable_pairs(dfi):
     dfi = dfi.drop(columns = ['fips'])
